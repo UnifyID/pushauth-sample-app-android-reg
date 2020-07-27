58f89e10d2bf90606bc3be07d096f22654163d5d
Tom Zhang <tzhang@Toms-MacBook-Pro.local>
2020-07-27 05:45:04 -0700

    Create button handler that initializes PushAuth

--- app/src/main/java/id/unify/pushauthreferenceapp/MainActivity.java
+++ app/src/main/java/id/unify/pushauthreferenceapp/MainActivity.java
@@ -3,6 +3,10 @@ package id.unify.pushauthreferenceapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,25 +24,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Temporarily hardcoded SDK key and user identifier
-        final String sdkKey = "https://be77a55d572220cd7180e5dc0460476d@config.unify.id";
-        final String user = "afkhadslfjsaf";
-
-        // Initialize an instance of the PushAuth SDK
-        List modules = new ArrayList<>();
-        modules.add(new UnifyIDPushAuthModule());
-
-        UnifyID.initialize(getApplicationContext(), sdkKey, user, modules, new CompletionHandler() {
+        Button confirmButton = (Button) findViewById(R.id.ConfirmButton);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCompletion() {
-                // Initialization successful
-                Log.d(TAG, "Initialization successful");
-            }
+            public void onClick(View view) {
+                EditText sdkKeyInput = (EditText) findViewById(R.id.SDKKeyInput);
+                String sdkKey = sdkKeyInput.getText().toString();
+                EditText userIDInput = (EditText) findViewById(R.id.UserIDInput);
+                String user = userIDInput.getText().toString();
 
-            @Override
-            public void onFailure(UnifyIDException e) {
-                // Initialization failed
-                Log.e(TAG, "Initialization failed", e);
+                // Initialize an instance of the PushAuth SDK
+                List modules = new ArrayList<>();
+                modules.add(new UnifyIDPushAuthModule());
+
+                UnifyID.initialize(getApplicationContext(), sdkKey, user, modules, new CompletionHandler() {
+                    @Override
+                    public void onCompletion() {
+                        // Initialization successful
+                        Log.d(TAG, "Initialization successful");
+                    }
+
+                    @Override
+                    public void onFailure(UnifyIDException e) {
+                        // Initialization failed
+                        Log.e(TAG, "Initialization failed", e);
+                    }
+                });
             }
         });
     }
